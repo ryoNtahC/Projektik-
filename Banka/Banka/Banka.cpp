75a6fbfa578518927e8d7a1f9c581e84b292c694
ryoN <91258360+ryoNtahC@users.noreply.github.com>
2023-01-02 18:43:40 +0100

    Vkladanie a vyberanie z učtu a aj prerobene vytvaranie
    kokos s týmto som sa hrozne dlho kašlal

--- Banka/Banka/Banka.cpp
+++ Banka/Banka/Banka.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int zostatok;
 char zoznam_uctov;
+
 int napis()
 
 {
@@ -23,9 +24,9 @@ int napis()
     return 0;
 }
 
-int main()
+void main()
 {
-    string main_menu[8] = { "1.Vytvorit ucet","2.Pozriet zostatok", "3.Vlozit peniaze ", "4.Vybrat peniaze", "5.Vymazat ucet", "6.Ukazat vsetky ucty","7.Quit" };
+    string main_menu[3] = { "1.Vytvorit ucet", "2.Ukazat vsetky ucty","3.Quit" };
     int volba_zaciatok;
     string meno;
     string admin = "Jozko";
@@ -33,12 +34,10 @@ int main()
 
     while (true)
     {
-                    napis();
-        for (int i = 0; i < 7; i++)
+        napis();
+        for (int i = 0; i < 3; i++)
         {
             cout << main_menu[i] << endl;
-
-
 
         }
 
@@ -48,12 +47,18 @@ int main()
         if (volba_zaciatok == 1)
         {
             system("CLS");
-            
+            napis();
+            int volba_c1;
+
+
+
             ofstream ZoznamUctov("ZoznamUctov.txt", ios_base::app);
+            ZoznamUctov << "Meno                      " << "Id                      " << "Pin                      " << "Zostatok" << endl;
             srand(time(NULL));
             int id[8] = {};
             string meno;
-            
+
+
             int pin;
 
 
@@ -61,17 +66,17 @@ int main()
             cin >> meno;
             cout << "Zadaj svoj PIN(4-ciferne cislo nezacinajuce sa nulou): ";
             cin >> pin;
-            if(pin < 1000){
+            if (pin < 1000) {
                 cout << "Zadaj prosim PIN nezacinajuci sa nulou. Dakujem :)";
                 cin >> pin;
             }
-            else if(pin > 9999){
+            else if (pin > 9999) {
                 cout << "Zadaj prosim stvorciferny PIN. Dakujem :)";
                 cin >> pin;
             }
             cout << "Zadaj svoj pociatocny zostatok: ";
             cin >> zostatok;
-            if(zostatok <= 0){
+            if (zostatok <= 0) {
                 cout << "Nemozes zalozit ucet bez zakladneho vkladu a nemozes vlozit zapornu sumu penazi, zadaj realnu sumu.";
                 cin >> zostatok;
             }
@@ -82,65 +87,99 @@ int main()
             for (int j = 0; j < 8; j++) {
                 cout << id[j];
             }
+            ZoznamUctov.open("ZoznamUctov.txt");
+            ZoznamUctov << meno << "                      " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << "                      " << pin << "                      " << zostatok << endl;
+            ZoznamUctov.close();
             cout << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "PIN:" << pin;
-            MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "�" << endl << "PIN: " << pin << endl;
-            ZoznamUctov << meno <<"                    "<< id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << "                     " << pin << "                    " << endl;
-            
+            MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "€" << endl << "PIN: " << pin << endl << endl;
+
+            string menu_c2[3] = { "1.Vlozit peniaze", "2.Vybrat peniaze", "3.Back" };
+            cout << endl;
+            system("CLS");
+            napis();
+
+            while (true)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    cout << menu_c2[i] << endl;
+
+                }
+
+                cout << "Vyber si moznost: " << endl;
+                cin >> volba_c1;
+
+                if (volba_c1 == 1)
+                {
+                    system("CLS");
+                    napis();
+                    int zadaj;
+                    cout << "Zadaj kolko chces vlozit: ";
+                    cin >> zadaj;
+                    zostatok = zostatok + zadaj;
+                    MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "€" << endl << "PIN: " << pin << endl;
+                    cout << zostatok;
+                    cout << endl;
+
+                }
+
+                if (volba_c1 == 2)
+                {
+                    system("CLS");
+                    napis();
+                    int zadaj;
+                    cout << "Zadaj kolko chces vybrat: ";
+                    cin >> zadaj;
+
+                    if (zostatok < zadaj)
+                    {
+                        cout << "Na svojom ucte nemas dostatok penazi" << endl;
+                    }
+                    else
+                    {
+                        zostatok = zostatok - zadaj;
+                        MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "€" << endl << "PIN: " << pin << endl << endl;
+                        cout << zostatok;
+                        cout << endl;
+
+
+                    }
+
+
+                }
+
+                if (volba_c1 == 3)
+                {
+                    system("CLS");
+                    ZoznamUctov.open("ZoznamUctov.txt");
+                    ZoznamUctov << meno << "                      " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << "                      " << pin << "                      " << zostatok << endl;
+                    ZoznamUctov.close();
+                    break;
+                    system("CLS");
+
+                }
+            }
 
 
 
 
         }
+
         else if (volba_zaciatok == 2)
         {
             system("CLS");
-            cout << "Kolko penazi chces vlozit: " << endl;
-
-            cout << endl;
-            
-
-        }
-        else if (volba_zaciatok == 3)
-        {
-            system("CLS");
-            cout << "vlozit peniaze" << endl;
-            cout << endl;
-            
-            
-
-        }
-        else if (volba_zaciatok == 4)
-        {
-            system("CLS");
-            cout << "vybrat peniaze" << endl;
-            cout << endl;
-            
-            
-
-        }
-        else if (volba_zaciatok == 5)
-        {
-            system("CLS");
-            cout << "vymazat ucet" << endl;
-            cout << endl;
-
-            
-
-        }
-        else if (volba_zaciatok == 6)
-        {
-            system("CLS");
+            napis();
             cout << "Zadaj adminske heslo: ";
             cin >> admin_kontrola;
             cout << endl;
-            
+
             if (admin_kontrola == admin)
             {
                 cout << "heslo je spravne" << endl;
                 cout << "prehlad vsetkych uctov" << endl;
                 cout << endl;
-                
-                
+
+
 
 
             }
@@ -148,21 +187,21 @@ int main()
             {
                 cout << "Smola heslo je nespravne" << endl;
                 cout << endl;
-                
+
             }
 
 
         }
-        else if (volba_zaciatok == 7)
+        else if (volba_zaciatok == 3)
         {
             system("CLS");
+            napis();
             cout << "Vytvorili: Matus Budos, Timotej Dolnik a Stanislav Chabrecek";
             cout << endl;
             break;
-            return 0;
 
         }
     }
-    
+
 }
 
