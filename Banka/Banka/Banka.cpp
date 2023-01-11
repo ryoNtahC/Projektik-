80f0c17e9486ed723b81560564ba5b91a095191d
ryoN <91258360+ryoNtahC@users.noreply.github.com>
2023-01-11 17:04:28 +0100

    Finally The End, Konec, Šlus
    Normalne som neveril ze sa tu raz dostanem

--- Banka/Banka/Banka.cpp
+++ Banka/Banka/Banka.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdlib.h>
 #include <fstream>
+#include <Windows.h>
 using namespace std;
 
 int zostatok;
@@ -11,15 +12,17 @@ char zoznam_uctov;
 int napis()
 
 {
-    cout  << "   I                                                             I   " << endl;
-    cout  << " aaaaa       aaaaaa    aaaaaa   a     a  a   a   aaaaaa        aaaaa " << endl;
-    cout  << "a  I  a      a     a  a      a  aa    a  a  a   a      a      a  I  a" << endl;
-    cout  << "a  I         a     a  a      a  a a   a  a a    a      a      a  I   " << endl;
-    cout  << " aaaaa       aaaaaa   aaaaaaaa  a  a  a  aa     aaaaaaaa       aaaaa " << endl;
-    cout  << "   I  a      a     a  a      a  a   a a  a a    a      a         I  a" << endl;
-    cout  << "a  I  a      a     a  a      a  a    aa  a  a   a      a      a  I  a" << endl;
-    cout  << " aaaaa       aaaaaa   a      a  a     a  a   a  a      a       aaaaa " << endl;
-    cout  << "   I                                                             I   " << endl;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h,6);
+    cout  << "              I                                                             I   " << endl;
+    cout  << "            aaaaa       aaaaaa    aaaaaa   a     a  a   a   aaaaaa        aaaaa " << endl;
+    cout  << "           a  I  a      a     a  a      a  aa    a  a  a   a      a      a  I  a" << endl;
+    cout  << "           a  I         a     a  a      a  a a   a  a a    a      a      a  I   " << endl;
+    cout  << "            aaaaa       aaaaaa   aaaaaaaa  a  a  a  aa     aaaaaaaa       aaaaa " << endl;
+    cout  << "              I  a      a     a  a      a  a   a a  a a    a      a         I  a" << endl;
+    cout  << "           a  I  a      a     a  a      a  a    aa  a  a   a      a      a  I  a" << endl;
+    cout  << "            aaaaa       aaaaaa   a      a  a     a  a   a  a      a       aaaaa " << endl;
+    cout  << "              I                                                             I   " << endl;
     cout << endl;
     cout << endl;
     return 0;
@@ -27,6 +30,7 @@ int napis()
 
 int main()
 {
+ 
     string main_menu[3] = { "1.Vytvorit ucet", "2.Ukazat vsetky ucty","3.Quit" };
     int volba_zaciatok;
     string meno;
@@ -38,16 +42,22 @@ int main()
         napis();
         for (int i = 0; i < 3; i++)
         {
-            cout << "                                     " << main_menu[i] << endl;
+            HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(h, 7);
+            cout << "                        " << main_menu[i] << endl;
 
         }
+        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(h, 7);
 
-        cout <<"                                     " << "Vyber si moznost: "<< endl;
-        cout << "                                     ";
+        cout <<"                        " << "Vyber si moznost: "<< endl;
+        cout << "                        ";
         cin >> volba_zaciatok;
 
         if (volba_zaciatok == 1)
         {
+            HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(h, 7);
             system("CLS");
             napis();
             int volba_c1;
@@ -62,22 +72,33 @@ int main()
 
             int pin;
 
-            cout << "Zadaj svoje meno(Spolu a bez medzier): ";
+            
+            SetConsoleTextAttribute(h, 7);
+
+            cout << "                        " << "Zadaj svoje meno(Spolu a bez medzier): ";
             cin >> meno;
-            cout << "Zadaj svoj PIN(4-ciferne cislo nezacinajuce sa nulou): ";
+            cout << "                        " << "Zadaj svoj PIN(4-ciferne cislo nezacinajuce sa nulou): ";
             cin >> pin;
             if (pin < 1000) {
-                cout << "Zadaj prosim PIN nezacinajuci sa nulou. Dakujem :)";
+                
+                SetConsoleTextAttribute(h, 7);
+                cout << "                        " << "Zadaj prosim PIN nezacinajuci sa nulou. Dakujem :)" << endl;
+                cout << "                        ";
                 cin >> pin;
+                
             }
             else if (pin > 9999) {
-                cout << "Zadaj prosim stvorciferny PIN. Dakujem :)";
+                
+                SetConsoleTextAttribute(h, 7);
+                cout << "                        " << "Zadaj prosim stvorciferny PIN. Dakujem :)";
                 cin >> pin;
             }
-            cout << "Zadaj svoj pociatocny zostatok: ";
+            cout << "                        " << "Zadaj svoj pociatocny zostatok: ";
             cin >> zostatok;
             if (zostatok <= 0) {
-                cout << "Nemozes zalozit ucet bez zakladneho vkladu a nemozes vlozit zapornu sumu penazi, zadaj realnu sumu.";
+                
+                SetConsoleTextAttribute(h, 7);
+                cout << "                        " << "Nemozes zalozit ucet bez zakladneho vkladu a nemozes vlozit zapornu sumu penazi, zadaj realnu sumu.";
                 cin >> zostatok;
             }
             cout << endl;
@@ -85,6 +106,7 @@ int main()
             for (int i = 0; i < 8; i++)
                 id[i] = 1 + (rand() % 8) + 1;
             for (int j = 0; j < 8; j++) {
+                SetConsoleTextAttribute(h, 7);
                 cout << id[j];
             }
             ZoznamUctov.open("ZoznamUctov.txt");
@@ -100,13 +122,16 @@ int main()
 
             while (true)
             {
+                SetConsoleTextAttribute(h, 7);
                 for (int i = 0; i < 3; i++)
                 {
-                    cout << menu_c2[i] << endl;
+                    SetConsoleTextAttribute(h, 7);
+                    cout << "                        " << menu_c2[i] << endl;
 
                 }
 
-                cout << "Vyber si moznost: " << endl;
+                cout << "                        " << "Vyber si moznost: " << endl;
+                cout << "                        ";
                 cin >> volba_c1;
 
                 if (volba_c1 == 1)
@@ -114,32 +139,44 @@ int main()
                     system("CLS");
                     napis();
                     int zadaj;
-                    cout << "Zadaj kolko chces vlozit: ";
+                    SetConsoleTextAttribute(h, 7);
+                    cout << "                        " << "Zadaj kolko chces vlozit: " << endl;
+                    cout << "                        ";
                     cin >> zadaj;
+                    cout << endl;
                     zostatok = zostatok + zadaj;
                     MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "€" << endl << "PIN: " << pin << endl;
-                    cout << zostatok;
+                    cout <<"                        " << zostatok << endl;
                     cout << endl;
+                    
 
                 }
 
                 if (volba_c1 == 2)
                 {
+
                     system("CLS");
                     napis();
                     int zadaj;
-                    cout << "Zadaj kolko chces vybrat: ";
+                    SetConsoleTextAttribute(h, 7);
+                    cout << "                        " << "Zadaj kolko chces vybrat: "<<endl;
+                    cout << "                        ";
                     cin >> zadaj;
 
                     if (zostatok < zadaj)
                     {
-                        cout << "Na svojom ucte nemas dostatok penazi" << endl;
+                        SetConsoleTextAttribute(h, 7);
+                        cout << endl;
+                        cout << "                        " << "Na svojom ucte nemas dostatok penazi" << endl;
+                        cout << endl;
                     }
                     else
                     {
+                        SetConsoleTextAttribute(h, 7);
                         zostatok = zostatok - zadaj;
                         MojaFile << "Meno: " << meno << endl << "Id: " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << endl << "Zostatok: " << zostatok << "€" << endl << "PIN: " << pin << endl << endl;
-                        cout << zostatok;
+                        cout << endl;
+                        cout << "                        " << zostatok << endl ;
                         cout << endl;
 
 
@@ -150,31 +187,36 @@ int main()
 
                 if (volba_c1 == 3)
                 {
+                    SetConsoleTextAttribute(h, 7);
                     system("CLS");
                     ZoznamUctov.open("ZoznamUctov.txt", ios::app);
-                    ZoznamUctov << meno << "                      " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << "                      " << pin << "                      " << zostatok << endl;
+                    ZoznamUctov << meno << "                    " << id[0] << id[1] << id[2] << id[3] << id[4] << id[5] << id[6] << id[7] << "                      " << pin << "                      " << zostatok << endl;
                     ZoznamUctov.close();
                     break;
                     system("CLS");
 
                 }
+
+         
+
             }
-
-
-
 
         }
 
         else if (volba_zaciatok == 2)
         {
+           
             system("CLS");
             napis();
-            cout << "Zadaj adminske heslo: ";
+            SetConsoleTextAttribute(h, 7);
+            cout << "                        " << "Zadaj adminske heslo: " << endl;
+            cout << "                        ";
             cin >> admin_kontrola;
             cout << endl;
 
             if (admin_kontrola == admin)
             {
+                
                 system("CLS");
                 napis();
 
@@ -187,9 +229,11 @@ int main()
 
 
                 int zasa_volba;
-                string choice[1] = { "1.Back", };
-                cout << "heslo je spravne" << endl << endl;
+                string choice[1] = { "Pre navrat do menu zmackni 1: ", };
+                SetConsoleTextAttribute(h, 10);
+                cout << "                        " << "heslo je spravne" << endl << endl;
                 while (getline(ZoznamUctov1, myText)) {
+                    SetConsoleTextAttribute(h, 7);
 
                     cout << myText << endl;
 
@@ -197,17 +241,19 @@ int main()
 
                 for (int i = 0; i < 1; i++)
                 {
-                    cout << choice[i] << endl;
+                    SetConsoleTextAttribute(h, 7);
+                    cout << endl;
+                    cout << "                        " << choice[i];
 
 
                 }
 
-                cout << "Vyber si moznost: " << endl;
                 cin >> zasa_volba;
 
 
                 if (zasa_volba = 1)
                 {
+                    SetConsoleTextAttribute(h, 7);
                     system("CLS");
                     napis();
                     system("CLS");
@@ -220,8 +266,9 @@ int main()
 
                 else
                 {
-                    cout << "zadal si neplatnu moznost" << endl;
-                    cin >> zasa_volba;
+                    SetConsoleTextAttribute(h, 7);
+                    cout << "                        " << "zadal si neplatnu moznost" << endl;
+                    break;
 
 
                 }
@@ -236,8 +283,15 @@ int main()
             }
             else
             {
-                cout << "Smola heslo je nespravne" << endl;
+                system("CLS");
+                SetConsoleTextAttribute(h, 4);
                 cout << endl;
+                cout << endl;
+                cout << endl;
+                cout << endl;
+                cout << "                        " << "Smola heslo je nespravne" << endl;
+                cout << endl;
+                break;
 
             }
 
@@ -245,13 +299,19 @@ int main()
         }
         else if (volba_zaciatok == 3)
         {
+        
             system("CLS");
             napis();
-            cout << "Vytvorili: Matus Budos, Timotej Dolnik a Stanislav Chabrecek";
+            SetConsoleTextAttribute(h, 3);
+            cout << "                        " << "Vytvorili: Matus Budos, Timotej Dolnik a Stanislav Chabrecek";
             cout << endl;
+            SetConsoleTextAttribute(h, 7);
             break;
 
+   
         }
+
+
     }
 
 }
